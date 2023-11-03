b7153a7994a34e502fb047e425628c61e573c86b
naolgithub <naolcovered@gmail.com>
2023-11-03 18:35:35 +0300

    constants are added

---
+++ how_to_test_a_flutter_app/lib/core/constants/constants.dart
@@ -0,0 +1,9 @@
+// const options = {
+//   method: 'GET',
+//   url: 'https://ajith-holy-bible.p.rapidapi.com/GetVerseOfaChapter',
+//   params: {Verse: '1', chapter: '1', Book: 'Luke'},
+//   headers: {
+//     'X-RapidAPI-Key': 'af28f1fa96msh59e84b698012ab8p1ad54bjsn36b4d1d1de81',
+//     'X-RapidAPI-Host': 'ajith-holy-bible.p.rapidapi.com'
+//   }
+// };
